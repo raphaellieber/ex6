e94b973ad12d75ebc38823c26dad9ecd6f7a87e6
Anton Kravtsov <anton.kravtsov@mail.huji.ac.il>
2023-01-12 21:24:37 +0200

    added classes

---
+++ src/oop/ex6/main/CorrectnessChecker.java
@@ -0,0 +1,57 @@
+package oop.ex6.main;
+
+// TYPES = ["int", "double", "String", "boolean", "char"]
+// var name - "^(?!\d)[_]{1,}\w+|[A-Za-z]+\w*"
+// int value - "^[-+]{0,1}\d+"
+// double value = "^[-+]{0,1}\d+.{0,1}\d*|[-+]{0,1}\d*.{0,1}\d+"
+// boolean value = "true|false|^[-+]{0,1}\d+.{0,1}\d*|[-+]{0,1}\d*.{0,1}\d+"
+// string value = "^"[\w\W]*""
+// char Value = "^'[\w\W]{1}'"
+
+public class CorrectnessChecker {
+
+    private static final String VAR_NAME= "^(?!\\d)_+\\w+|[A-Za-z]+\\w*";
+    private static final String INT_VALUE = "^(?!\\d)_+\\w+|[A-Za-z]+\\w*";
+    private static final String DOUBLE_VALUE = "^[-+]?\\d+.?\\d*|[-+]?\\d*.?\\d+";
+    private static final String BOOLEAN_VALUE = "true|false|^[-+]?\\d+.?\\d*|[-+]?\\d*.?\\d+";
+    private static final String STRING_VALUE = "^\"[\\w\\W]*\"";
+    private static final String CHAR_VALUE = "^'[\\w\\W]'";
+
+    private static final String INT = "int";
+    private static final String DOUBLE = "double";
+    private static final String BOOLEAN = "boolean";
+    private static final String STRING = "String";
+    private static final String CHAR = "char";
+
+
+    /**
+     * A function that checks the var name correctness of a given name
+     * @param name represents the given name
+     * @return ture if correct, false otherwise
+     */
+    public static boolean checkVarName(String name) {return name.matches(VAR_NAME); }
+
+    /**
+     * A function that checks the value correctness depends on a type
+     * @param type represents the type, can get: int, double, boolean, String, char
+     * @param value represents the value
+     * @return true if correct, false otherwise (as well if the given type don't match)
+     */
+    public static boolean checkValue(String type, String value) {
+
+        switch (type){
+            case INT: return value.matches(INT_VALUE);
+            case DOUBLE: return value.matches(DOUBLE_VALUE);
+            case BOOLEAN: return value.matches(BOOLEAN_VALUE);
+            case STRING: return value.matches(STRING_VALUE);
+            case CHAR: return value.matches(CHAR_VALUE);
+        }
+
+        // if the type didn't match
+        return false;
+    }
+
+
+
+
+}
--- src/oop/ex6/main/VarTables.java
+++ src/oop/ex6/main/VarTables.java
@@ -8,6 +8,8 @@ public class VarTables {
     private static final String FINAL = "FINAL";
     private static final String NOT_FINAL = "NOT_FINAL";
 
+    private static final int ARRAY_SIZE = 3;
+
     private static final int FINAL_LOC = 0;
     private static final int TYPE_LOC = 1;
     private static final int VALUE_LOC = 2;
@@ -16,16 +18,16 @@ public class VarTables {
     private static final String TYPE = "TYPE";
     private static final String FINAL_OR_NOT = "FINAL_OR_NOT";
     private static final String VALUE = "VALUE";
-    private static final String REGEX = "REGEX";
+//    private static final String REGEX = "REGEX";
 
     // name - [FINAL/NOT, Type, Regex, Value]
-    private final HashMap<String, String[]> globalMemberVars;
+    private final HashMap<String, String[]> globalVars;
     private final LinkedList<HashMap<String, String[]>> scopeVars;
 
     private int scopeInd;
 
     VarTables(){
-        this.globalMemberVars = new HashMap<>();
+        this.globalVars = new HashMap<>();
         this.scopeVars = new LinkedList<>();
         this.scopeInd = 0;
     }
@@ -36,13 +38,12 @@ public class VarTables {
      * @param globOrLoc a boolean that represents if a var is global/member var (true) or local (false)
      * @param finalOrNot a boolean that represents if a var is final var (true) or not (false)
      * @param type String that represents vars type
-     * @param value String that represents vars value
+     * @param value String that represents vars value, may be null
      * @return true upon success, false otherwise (if the var got incorrect value)
      */
     public boolean addVar(String name, boolean globOrLoc, boolean finalOrNot, String type, String value){
-        // todo need to add regex check to the value
 
-        String[] varArray = new String[4];
+        String[] varArray = new String[ARRAY_SIZE];
 
         // Final or not:
         if (finalOrNot) { varArray[0] = FINAL;}
@@ -53,7 +54,7 @@ public class VarTables {
         varArray[VALUE_LOC] = value;
 
         // if global or member var
-        if (globOrLoc) { this.globalMemberVars.put(name, varArray);}
+        if (globOrLoc) { this.globalVars.put(name, varArray);}
 
         // if local var
         else {
@@ -72,11 +73,10 @@ public class VarTables {
 
     /**
      * A method that returns the wanted info about the var
+     * !!!method assumes the var is in the table and the infoType is correct!!!!
      * @param name a String that represents the vars name
      * @param infoType represents the wanted info, can get: "TYPE" "FINAL_OR_NOT" "VALUE" "REGEX"
-     *                 if none of them, returns false
-     * @return returns the wanted info if the car is in the tables and if the wanted info was correct
-     *         false otherwise
+     * @return returns the wanted info of the var from the tables
      */
     public String getVarInfo (String name, String infoType) {
 
@@ -84,8 +84,8 @@ public class VarTables {
         if (infoType.equals(TYPE)) { inVarLocation = TYPE_LOC;}
         if (infoType.equals(FINAL_OR_NOT)) { inVarLocation = FINAL_LOC;}
         if (infoType.equals(VALUE)) { inVarLocation = VALUE_LOC;}
-        if (infoType.equals(REGEX)) { inVarLocation = REGEX_LOC;}
-        if (inVarLocation == -1) {return null;}
+//        if (infoType.equals(REGEX)) { inVarLocation = REGEX_LOC;}
+        if (inVarLocation == -1) {return null;} // should not get here! // todo maybe to raise exception??
 
         // first checking in the local scopes
         for (HashMap<String, String[]> map :this.scopeVars) {
@@ -95,11 +95,11 @@ public class VarTables {
         }
 
         // checking in the global / members table
-        for (Map.Entry<String, String[]> entry: this.globalMemberVars.entrySet()){
+        for (Map.Entry<String, String[]> entry: this.globalVars.entrySet()){
             if (entry.getKey().equals(name)) { return entry.getValue()[VALUE_LOC]; }
         }
 
-        // if not found
+        // should not get here! // todo maybe to raise exception??
         return null;
     }
 
@@ -115,4 +115,28 @@ public class VarTables {
         this.scopeInd --;
         this.scopeVars.removeLast();
     }
+
+    /**
+     * A method that checks if a var is in the tables
+     * @param name represents the name of the var
+     * @return true if is, false otherwise
+     */
+    public boolean varDeclared(String name) {
+
+        // first checking in the local scopes
+        for (HashMap<String, String[]> map :this.scopeVars) {
+            if (map.containsKey(name)) { return true;}
+        }
+
+        // then checking in the global/member table
+        return this.globalVars.containsKey(name);
+    }
 }
+
+// TYPES = ["int", "double", "String", "boolean", "char"]
+// var name - "^(?!\d)[_]{1,}\w+|[A-Za-z]+\w*"
+// int value - "^[-+]{0,1}\d+"
+// double value = "^[-+]{0,1}\d+.{0,1}\d*|[-+]{0,1}\d*.{0,1}\d+"
+// boolean value = "true|false|^[-+]{0,1}\d+.{0,1}\d*|[-+]{0,1}\d*.{0,1}\d+"
+// string value = "^"[\w\W]*""
+// char Value = "^'[\w\W]{1}'"
