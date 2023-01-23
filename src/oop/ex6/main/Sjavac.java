1ea470ffb24798735d396fa8c7ef58206de542ea
Anton Kravtsov <anton.kravtsov@mail.huji.ac.il>
2023-01-23 16:35:32 +0200

    now done for real

--- src/oop/ex6/main/Sjavac.java
+++ src/oop/ex6/main/Sjavac.java
@@ -17,6 +17,11 @@ public class Sjavac {
     static final String FILE_NOT_FOUND = "File was not found or can't be open";
     static final String NO_PATH = "No file path given";
 
+    /**
+     * A function the compiles the file at a given path
+     * @param filePath represents the given path
+     * @return 0 for success, 1 for failure and 2 for IO error
+     */
     public static int compileFile(String filePath) {
         try (  FileReader fileReader = new FileReader(filePath);
                FileReader firstRunFileReader = new FileReader(filePath);
